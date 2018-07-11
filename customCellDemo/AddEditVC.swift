7d81ead91f9d2e5873796d20be40dc5cc3f081d7
jupterorbita <john.williams@outlook.com>
2018-07-11 13:52:17 -0700

    init commit

---
+++ customCellDemo/AddEditVC.swift
@@ -0,0 +1,35 @@
+//
+//  AddEditVC.swift
+//  customCellDemo
+//
+//  Created by J on 7/11/2018.
+//  Copyright © 2018 Jman. All rights reserved.
+//
+
+import UIKit
+
+class AddEditVC: UIViewController {
+
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var ratingTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+
+    //cancel button
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // done button
+    @IBAction func savePressed(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "UnwindToVC", sender: self)
+    }
+    
+}
---
+++ customCellDemo/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  customCellDemo
+//
+//  Created by J on 7/11/2018.
+//  Copyright © 2018 Jman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableData: [Review] = []
+    
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "AddEditSegue", sender: sender)
+    }
+    
+    @IBAction func unwindToVC(segue: UIStoryboardSegue) {
+        let src = segue.source as! AddEditVC // gain access to other
+        let title = src.titleTextField.text
+        let rating = src.titleTextField.text
+        let notes = src.notesTextView.text
+        print(title, rating, notes)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewCell", for: indexPath)
+        
+        let review = tableData[indexPath.row]
+        cell.textLabel?.text = review.title
+        cell.detailTextLabel?.text = "\(review.rating)"
+
+        return cell
+    }
+}
